c3ca08f6c0bea0065fb18945cc312f5cbca1dfa2
andersonl24 <andersonl24@hsnet.ahsd.org>
2023-04-24 10:38:05 -0400

    Testing commit menu

---
+++ app/src/main/java/com/example/finalproject/TeamStatsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.finalproject.databinding.FragmentMenuBinding
+import com.example.finalproject.databinding.FragmentTeamStatsBinding
+
+class TeamStatsFragment : Fragment() {
+    private var _binding: FragmentTeamStatsBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTeamStatsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
