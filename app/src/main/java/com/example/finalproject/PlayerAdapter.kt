bf08766ca1a42a37e4d65c6d890abd51e3896e86
andersonl24 <andersonl24@hsnet.ahsd.org>
2023-05-10 10:39:31 -0400

    Testing commit menu Themes data class ui lot data schedule color fix binding in holder Finish Adapter pics

--- app/src/main/java/com/example/finalproject/PlayerAdapter.kt
+++ app/src/main/java/com/example/finalproject/PlayerAdapter.kt
@@ -1,19 +1,23 @@
 package com.example.finalproject
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.databinding.TeamInfoItemLayoutBinding
 
 class PlayerAdapter (val playerList:List<Player>):RecyclerView.Adapter<TeamInfoHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamInfoHolder {
+        val binding=TeamInfoItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return TeamInfoHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TeamInfoHolder, position: Int) {
-        TODO("Not yet implemented")
+val currentPlayer=playerList[position]
+        holder.bindPlayer(currentPlayer)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+return playerList.size
     }
 
 }
--- app/src/main/java/com/example/finalproject/TeamInfoFragment.kt
+++ app/src/main/java/com/example/finalproject/TeamInfoFragment.kt
@@ -7,31 +7,33 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.finalproject.databinding.FragmentMenuBinding
 import com.example.finalproject.databinding.FragmentTeamInfoBinding
+import com.example.finalproject.databinding.TeamInfoItemLayoutBinding
 
 class TeamInfoFragment : Fragment() {
-    private var _binding: FragmentTeamInfoBinding? = null
+    private var _binding: TeamInfoItemLayoutBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentTeamInfoBinding.inflate(inflater, container, false)
-        val players=listOf(Player("Mason Fedor","Junior","Forward","6\'4\"","180",0,10),
+        _binding = TeamInfoItemLayoutBinding.inflate(inflater, container, false)
+        val players=listOf(
+            Player("Mason Fedor","Junior","Forward","6\'4\"","180",R.drawable.patcropped,10),
             Player("Matt Show","Senior","Guard","5\'11\"","165",0,11)
-            ,Player("Will Marion","Junior","Guard","6\'1\"","1",0,12)
-            ,Player("Patrick Walsh","Senior","Guard/Forward","6\'0\"","1",0,22)
-            ,Player("Ryan Nealon","Junior","Guard","6\'1\"","1",0,23)
+            ,Player("Will Marion","Junior","Guard","6\'1\"","165",0,12)
+            ,Player("Patrick Walsh","Senior","Guard/Forward","6\'0\"","170",0,22)
+            ,Player("Ryan Nealon","Junior","Guard","6\'1\"","165",0,23)
             ,Player("Roman Cutrufello","Senior","Guard","5\'11\"","160",0,24)
-            ,Player("Lincoln Anderson","Junior","Guard","6\'0\"","1",0,25)
-            ,Player("Gene Curtin","Junior","Guard/Forward","6\'2\"","1",0,33)
-            ,Player("Jordan Shaffer","Freshman","Guard","6\'2\"","1",0,35)
-            ,Player("Zach Brister","Senior","Forward","6\'2\"","1",0,40)
-            ,Player("Jack Petrillo","Senior","Forward","6\'2\"","1",0,42)
+            ,Player("Lincoln Anderson","Junior","Guard","6\'0\"","155",0,25)
+            ,Player("Gene Curtin","Junior","Guard/Forward","6\'2\"","155",0,33)
+            ,Player("Jordan Shaffer","Freshman","Guard","6\'2\"","150",0,35)
+            ,Player("Zach Brister","Senior","Forward","6\'2\"","180",0,40)
+            ,Player("Jack Petrillo","Senior","Forward","6\'2\"","205",0,42)
             ,Player("Robby Lucas","Sophmore","Forward","6\'2\"","185",0,44))
 
 
-            val myAdapter=PlayerAdapter(players)
-        binding.recyclerView2.adapter=myAdapter
+            val myAdapter2=PlayerAdapter(players)
+        binding.recyclerVieww.adapter=myAdapter2
         return binding.root
     }
 
