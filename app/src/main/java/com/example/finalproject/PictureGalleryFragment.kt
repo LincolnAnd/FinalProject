6c67bb708891103cf8bf058d1d5ea0f320e77b97
andersonl24 <andersonl24@hsnet.ahsd.org>
2023-05-30 10:38:12 -0400

    Testing commit menu Themes data class ui lot data schedule color fix binding in holder Finish Adapter pics problem team stats photo next fix audio. keeps playing after leaving screen fix passing info to other screen fix image fix intent thingy

--- app/src/main/java/com/example/finalproject/PictureGalleryFragment.kt
+++ app/src/main/java/com/example/finalproject/PictureGalleryFragment.kt
@@ -1,5 +1,7 @@
 package com.example.finalproject
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.finalproject.databinding.FragmentMenuBinding
 import com.example.finalproject.databinding.FragmentPictureGalleryBinding
+
 
 class PictureGalleryFragment : Fragment() {
     private var _binding: FragmentPictureGalleryBinding? = null
@@ -16,8 +19,37 @@ class PictureGalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentPictureGalleryBinding.inflate(inflater, container, false)
+        val myOnClickListener: View.OnClickListener = View.OnClickListener { view ->
+            when(view.id) {
+               
+                R.id.button->openWebPage("https://bdnjphoto.com/20221208tahvbb")
+                R.id.button2->openWebPage("https://bdnjphoto.com/20221214ahnbb")
+                R.id.button3->openWebPage("https://bdnjphoto.com/20221229ahebbb")
+                R.id.button4->openWebPage("https://bdnjphoto.com/20230106ahpbbb")
+                R.id.button5->openWebPage("https://bdnjphoto.com/20230110ahvvbbb")
+                R.id.button6->openWebPage("https://bdnjphoto.com/20230120ahwsbbb")
+                R.id.button7->openWebPage("https://bdnjphoto.com/20230124ahhbbb")
+                R.id.button8->openWebPage("https://bdnjphoto.com/20230127spahbbb")
+
+            }}
+        binding.button.setOnClickListener(myOnClickListener)
+        binding.button2.setOnClickListener(myOnClickListener)
+        binding.button3.setOnClickListener(myOnClickListener)
+        binding.button4.setOnClickListener(myOnClickListener)
+        binding.button5.setOnClickListener(myOnClickListener)
+        binding.button6.setOnClickListener(myOnClickListener)
+        binding.button7.setOnClickListener(myOnClickListener)
+        binding.button8.setOnClickListener(myOnClickListener)
+
 
         return binding.root
+    }
+    fun openWebPage(url: String) {
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
